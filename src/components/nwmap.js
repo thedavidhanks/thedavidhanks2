e9bbec40c3909290bee7829a2bbdf33c70c9dee6
David Hanks <dphanks@gmail.com>
2019-03-07 21:42:56 -0600

    update map coordinate system

--- src/components/nwmap.js
+++ src/components/nwmap.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { ImageOverlay, Map, Marker, Popup, CRS } from 'react-leaflet';
-import NWtileMap from '../images/CombinedNW.png';
+import { ImageOverlay, Map, Marker, Popup } from 'react-leaflet';
+import { CRS } from 'leaflet';
+import MapImage from '../images/CombinedNW.png';
+//import MapImage from '../images/FNmap.jpg';
 
 class NWmap extends Component {
    constructor(){
@@ -11,24 +13,29 @@ class NWmap extends Component {
             lng: 0,
             zoom: 2
         };
-    }   
+    }
+    onDoubleClick = (e) => {
+        console.log(e.latlng);
+    }
     render(){
         const position = [this.state.lat, this.state.lng];
         const overstyle = {
             border: '1px solid',
             width: '100%'
         };
-        const bounds = [[-500,-500],[500,500]];
-        const mapBounds = [[-200,-200],[200,200]];
+        const bounds = [[-500,-500],[500,500]];      
         return (
             <div style={overstyle}>
                 <h4>New World</h4>
                 <Map 
                     center={position}
                     zoom={this.state.zoom}
+                    crs={CRS.Simple}
+                    doubleClickZoom={false}
+                    dblclick={this.onDoubleClick}
                     >
                   <ImageOverlay
-                    url={NWtileMap}
+                    url={MapImage}
                     bounds={bounds}
                   />
                   <Marker position={position}>
