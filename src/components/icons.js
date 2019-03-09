7855f0385e8e8ecee48a184e5d57b23404145816
David Hanks <dphanks@gmail.com>
2019-03-09 17:08:36 -0600

    Working with new markers

---
+++ src/components/icons.js
@@ -0,0 +1,25 @@
+ import L from 'leaflet'; 
+ 
+ export const iconConstruction = new L.Icon({
+    iconUrl: '../images/map-icons/construction.png',
+    iconSize: [32, 14],
+    iconAnchor: [16, 7],
+    popupAnchor: [16, 14],
+    tooltipAnchor: [16, 14]
+});
+
+ export const iconLandmark = new L.Icon({
+    iconUrl: '../images/map-icons/landmark.png',
+    iconSize: [32, 32],
+    iconAnchor: [16, 0],
+    popupAnchor: [16, 32],
+    tooltipAnchor: [16, 32]
+});
+
+ export const iconPeak = new L.Icon({
+    iconUrl: '../images/map-icons/peak.png',
+    iconSize: [32, 32],
+    iconAnchor: [16, 0],
+    popupAnchor: [16, 32],
+    tooltipAnchor: [16, 32]
+});
--- src/components/nwmap.js
+++ src/components/nwmap.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import { ImageOverlay, Map, Marker, Popup } from 'react-leaflet';
-import { CRS } from 'leaflet';
+import { CRS, Icon } from 'leaflet';
+
 import MapImage from '../images/CombinedNW.png';
+import { iconConstruction, iconLandmark } from './icons.js';
 //import MapImage from '../images/FNmap.jpg';
 
+const NewMarker =  props => {
+    return (
+        <Marker 
+            icon={iconLandmark}
+            position={props.position}>
+            <Popup position={props.position}>
+              A new Marker!!
+            </Popup>
+        </Marker>
+    ); 
+};
 class NWmap extends Component {
    constructor(){
         super();
 
         this.state = {
+            newMarker: false,
             lat: 0,
             lng: 0,
             zoom: 2
@@ -16,6 +30,15 @@ class NWmap extends Component {
     }
     onDoubleClick = (e) => {
         console.log(e.latlng);
+        
+        //add a marker to the coords
+        this.setState({
+            newMarker: true,
+            newMarkerLatLong: e.latlng
+        });
+        //open a config dialog
+        
+        //close or lose focus, save the point.
     }
     render(){
         const position = [this.state.lat, this.state.lng];
@@ -32,7 +55,7 @@ class NWmap extends Component {
                     zoom={this.state.zoom}
                     crs={CRS.Simple}
                     doubleClickZoom={false}
-                    dblclick={this.onDoubleClick}
+                    ondblclick={this.onDoubleClick}
                     >
                   <ImageOverlay
                     url={MapImage}
@@ -43,6 +66,7 @@ class NWmap extends Component {
                       A pretty CSS3 popup. <br /> Easily customizable.
                     </Popup>
                   </Marker>
+                  {this.state.newMarker ? <NewMarker position={this.state.newMarkerLatLong} /> : null}
                 </Map>
                 
             </div>
