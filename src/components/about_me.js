f5d594a56716e9f2632734dda2fefa207d2eee1e
David Hanks <dphanks@gmail.com>
2020-12-29 18:12:53 -0500

    about me updates wip

--- src/components/about_me.js
+++ src/components/about_me.js
@@ -1,132 +1,143 @@
-import React, { Component } from 'react';
-
-import ResumeCompany from './resume/ResumeCompany';
-import ResumePosition from './resume/ResumePosition';
-import profile from '../images/profile.jpg'
+import React, {useState, useEffect}  from 'react';
+import Select from 'react-select';
 
 const socialBlock = {
-    fontSize: "48px"
+    fontSize: "48px", 
+    margin: "5px"
 };
-class AboutMe extends Component {
-  constructor(props){
-        super(props);
+const AboutMe = () => {
+    const skillscategories = [
+        {value: 'developer', label: 'developer'},
+        {value: 'ide', label: 'ide'},
+        {value: 'vcs', label: 'vcs'},
+        {value: 'language', label: 'progamming language'},
+        {value: 'cad', label: 'CAD'},
+        {value: 'analysis', label: 'analysis'},
+        {value: 'jslibraries', label: 'Libraries'}
+    ]
 
-        this.state = { 
-            mechCheck: "",
-            softCheck: ""
-        };
+    const [skills, setSkills] = useState(skillscategories);
+    const [skillsDOM, setSkillsDOM] = useState();
+    useEffect( () =>{
+        console.log("skills changed");
+        if(skillsDOM){
+
+            //reset all children to dimished clash
+            [...skillsDOM.children].forEach( child =>{
+                child.classList.remove("highlightSkill");
+                child.classList.add("diminishSkill");
+            });
+
+            //only highlight selected.
+            if(skills){
+                skills.forEach( skill =>{
+                    [...skillsDOM.children].forEach( child =>{
+                        if(child.classList.contains(skill.value)){
+                            child.classList.add("highlightSkill");
+                            child.classList.remove("diminishSkill");
+                        }
+                    });
+                })
+            }
+            
+        }else{
+            console.log("no skills")
+        }
+    }, [skills]);
+
+    const updateSkills = (value) => {
+        setSkills(value);
     }
-    handleInputChange = (e) => {
-        const target = e.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
-        this.setState({
-            [target.name] : value
-        });
-    }
-  render(){
+    
     return (
         <div className="resume">
-            <div className="row block-links">
-                <div className="col-1 profileDiv text-left border">
-                            <img src={profile} 
-                                className="profileImg" 
-                                alt="David Hanks"
-                            />
-                        </div>
-                        <div className="col-1 socialDiv">
-                            <a className="fa fa-linkedin-square" style={socialBlock} href="https://www.linkedin.com/in/thedavidhanks"></a><br />
-                            <a className="fa fa-github-square" style={socialBlock} href="https://github.com/thedavidhanks/"></a>
-                        </div>
-                        <div className="col-10">
-                        </div>
-            </div>    
-            <hr />
+
             <div className="row">
-                <div className="col"></div>
-                <div className="ck-button col-md-1">
-                    <label>
-                        <input hidden name="mechCheck" type="checkbox" checked={this.state.mechCheck} onChange={this.handleInputChange} />
-                        <span>Mechanical</span>
-                    </label>
+                <div className="col-md-8 ">
+                    <h2>About me.</h2>
+                    <p>I'm an engineer, developer, tinkerer, climber, ultimate freesbie player, eagle scout, traveller, native Cajun and husband.  
+                        My wife and I moved to Chattanooga, TN from Houston, TX after an 8 month road trip around the US.  I'm exploring what the beautiful country-side has to offer
+                        while attempting to lay down some roots.  </p>
+                    <p>In 2002 I graduated from the University of Louisiana with a BS in Mechanical Engineering. I've spent the
+                         majority of my career developing and maintaining oil and gas product lines.  Specifically I've worked on tongs/slips, Blow-out preventers, and supporting
+                         controls.  I've been able to experince the full range of product life cycle: from developing a specification with a customer, through creating/reviewing 
+                         drawings, analyizing loads, writing and conducting tests, and installing equipment on-site.
+                    </p>
+                    <p>
+                        In more recent years, I've turned my attention to developing software tools.  
+                    </p>
+                    <h3>Me and Software Development</h3>
+                    <p>I've been developing software since my family got an Apple IIgs.  
+                        I've programmed in a plethora of languages (C, C++, Java, Python, NodeJS, javaScript, VB, PHP, Pascal, BASIC)
+                    </p>
+                    <h3>In case you were looking for a developer...</h3>
+                    <p>You may have landed here looking for a seasonsed Sr. Software Architech with 10+ yrs experience.  This is not me.  </p>
                 </div>
-                <div className="ck-button col-md-1">        
-                    <label>
-                        <input hidden name="softCheck" type="checkbox" checked={this.state.softCheck} onChange={this.handleInputChange} />
-                        <span>Software</span>
-                    </label>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col">Let me tell you about me and what I'm looking for...</div>
-            </div>
-            <div className="row">
-                <div className="col-md-9 workExperience">
-                    <div className="header2">Work Experiences</div>
-                    <ResumeCompany company="Offshore Technical Solutions" start="Sept 2015" end="present">
-                        <ResumePosition title="Senior Professional Engineer">
-                            <ul>
-                                <li>Developed web based form utilizing <ToggleHighlight software={true} showSoftware={this.state.softCheck} showMechanical={this.state.mechCheck}>React, jQuery, AJAX, PHP, and mySQL</ToggleHighlight></li>
-                                <li><ToggleHighlight mechanical={true} showSoftware={this.state.softCheck} showMechanical={this.state.mechCheck}>Performed analysis</ToggleHighlight> for offshore vessels required by the US government.</li>
-                                <li>Evaluated hydraulic safety systems on drilling rigs as required by regulations</li>
-                            </ul>
-                        </ResumePosition>
-                    </ResumeCompany>
-                    <ResumeCompany company="Oceaneering" start="Oct 2014" end="May 2015">
-                        <ResumePosition title="Senior Subsea Controls Engineer">
-                            <ul>
-                                <li>Lead a team of engineers and designers in the development of blow out preventer hydraulic control systems</li>
-                                <li>Reviewed drawings, Solidworks models and calculations for the release of new product</li>
-                            </ul>
-                        </ResumePosition>
-                    </ResumeCompany>
-                    <ResumeCompany company="General Electric" start="May 2006" end="Sept 2014">
-                        <ResumePosition title="Senior Subsea Controls Engineer, New Product Introduction">
-                        <ul>
-                            <li>Lead design and development of the next generation electro/hydraulic subsea control pod through writing specifications, reviewing drawings, analysis, selecting components, developing testing, and tracking progress.</li>
-                            <li>Presented design reviews to chief engineers, manufacturing, and sourcing.</li>
-                        </ul>
-                        </ResumePosition>
-                        <ResumePosition title="Lead Product Engineer, Subsea Controls">
-                            <ul>
-                                <li>Selected or modified hydraulic control components for new systems</li>
-                                <li>Worked closely with manufacturing, designers, planners, vendors, and clients to meet deliverables</li>
-                                <li>Created and coordinated tests of new product</li>
-                                <li>Responded to manufacturing concerns related to production</li>
-                                <li>Mentored and trained new engineers</li>
-                            </ul>
-                        </ResumePosition>
-                        <ResumePosition title="Field Engineer">
-                            <ul>
-                                <li>Commissioned GE control systems</li>
-                                <li>Conducted troubleshooting and repairs on hydraulic control systems</li>
-                            </ul>
-                        </ResumePosition>
-                    </ResumeCompany>
-                </div>
-                <div className="col-md-3 certificates resumeBorder">
+                <div className="col-md-4 certificates resumeBorder">
+                    <h5>Resumes</h5>
+                    <ul>
+                        <li><a href="https://tdh-public-files.s3.us-east-2.amazonaws.com/resume/SeniorMechanicalEngineer.pdf" target="_blank" rel="noopener noreferrer">Sr. Mechanical Engineer</a></li>
+                        <li><a href="https://tdh-public-files.s3.us-east-2.amazonaws.com/resume/SoftwareDeveloper.pdf" target="_blank" rel="noopener noreferrer">Software Developer</a></li>
+                    </ul>
                     <h5>Certificates</h5>
                     <ul>
-                        <li><a href="https://www.udemy.com/certificate/UC-U878XCQQ/">React/Redux</a></li>
-                        <li><a href="https://www.udacity.com/course/java-programming-basics--ud282?utm_campaign=android_pathway&utm_medium=referral&utm_source=aws_s3_pdf">Java</a></li>
-                        <li><a href="https://courses.edx.org/certificates/5906b12c3b864b4a9301b1b02d2f867f">jQuery</a></li>
-                        <li><a href="https://www.udemy.com/git-bootcamp-with-github-learn-step-by-step/learn/v4/content">Git</a></li>
-                        <li><a href="https://courses.edx.org/certificates/1a98e664ec694a7ea85a5a9117548ac0">HTML/CSS</a></li>
+                        <li><a href="https://engineers.texas.gov/roster/pesearch.html##result-top" target="_blank" rel="noopener noreferrer">P.E. (104644)</a></li>
+                        <li><a href="">AWS CCP (coming soon)</a></li>
+                        <li>Eagle Scout</li>
+                        
                     </ul>
-                    
+                    <h5>Coursework</h5>
+                    <ul>
+                        <li><a href="http://omscs.gatech.edu/cs-6250-computer-networks" target="_blank" rel="noopener noreferrer">Computer Networks</a></li>
+                        <li><a href="http://omscs.gatech.edu/cs-6200-introduction-operating-systems" target="_blank" rel="noopener noreferrer">Intro to OS</a></li>
+                        <li><a href="http://omscs.gatech.edu/cs-6300-software-development-process" target="_blank" rel="noopener noreferrer">Software Dev Process</a></li>
+                        <li><a href="https://www.udemy.com/certificate/UC-U878XCQQ/" target="_blank" rel="noopener noreferrer">React/Redux</a></li>
+                        <li><a href="https://www.udacity.com/course/java-programming-basics--ud282?utm_campaign=android_pathway&utm_medium=referral&utm_source=aws_s3_pdf" target="_blank" rel="noopener noreferrer">Java</a></li>
+                        <li><a href="https://courses.edx.org/certificates/5906b12c3b864b4a9301b1b02d2f867f" target="_blank" rel="noopener noreferrer">jQuery</a></li>
+                        <li><a href="https://www.udemy.com/git-bootcamp-with-github-learn-step-by-step/learn/v4/content" target="_blank" rel="noopener noreferrer">Git</a></li>
+                        <li><a href="https://courses.edx.org/certificates/1a98e664ec694a7ea85a5a9117548ac0" target="_blank" rel="noopener noreferrer">HTML/CSS</a></li>
+                    </ul>
+                    <h5>Skills</h5>
+                    <Select 
+                        options={skillscategories}
+                        isMulti
+                        defaultValue={skills}
+                        name="skills"
+                        onChange={updateSkills}
+                    />
+                    <div id="skillList" ref={(skillsList) => { setSkillsDOM(skillsList)}}>
+                        <span className="cad highlightSkill">Solidworks, </span>
+                        <span className="cad highlightSkill">AutoCAD, </span>
+                        <span className="cad highlightSkill">Fusion360, </span>
+                        <span className="cad highlightSkill">Inventor,  </span>
+                        <span className="cad highlightSkill">Pro/E, </span>
+                        <span className="analysis highlightSkill">Ansys, </span>
+                        <span className="ide developer highlightSkill">Visual Studio Code, </span>
+                        <span className="ide developer highlightSkill">Visual Studio, </span>
+                        <span className="ide developer highlightSkill">NetBeans, </span>
+                        <span className="vcs developer highlightSkill">git, </span>
+                        <span className="vcs developer highlightSkill">github, </span>
+                        <span className="language developer highlightSkill">python, </span>
+                        <span className="language developer highlightSkill">C, </span>
+                        <span className="language developer highlightSkill">C++, </span>
+                        <span className="language developer highlightSkill">Java, </span>
+                        <span className="language developer highlightSkill">javascript, </span>
+                        <span className="language developer highlightSkill">NodeJS, </span>
+                        <span className="jslibraries developer highlightSkill">ReactJS, </span>
+                        <span className="jslibraries developer highlightSkill">Leaflet, </span>
+                        <span className="jslibraries developer highlightSkill">Bootstrap</span>
+                    </div>
+                     
+
+                    <div className="socialDiv">
+                        <a className="fa fa-linkedin-square" style={socialBlock} href="https://www.linkedin.com/in/thedavidhanks" target="_blank" rel="noopener noreferrer"></a>
+                        <a className="fa fa-github-square" style={socialBlock} href="https://github.com/thedavidhanks/" target="_blank" rel="noopener noreferrer"></a>
+                        <a className="fa fa-facebook-square" style={socialBlock} href="https://www.facebook.com/david.hanks" target="_blank" rel="noopener noreferrer"></a>
+                    </div>
                 </div>    
             </div>
-            
-            
         </div>
                 
     );
-  }  
-};
-
-const ToggleHighlight = (props) => {
-    const highlight = (props.software && props.showSoftware) || (props.mechanical && props.showMechanical) ? true : false;
-    return <span className={ highlight ? "highlight" : ""}>{props.children}</span>;
-    
-};
-
+} 
 export default AboutMe;
